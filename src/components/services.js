13a31becf293975e15dd8f0fe43d6023b9c007dd
grantposell <grantposell@gmail.com>
2019-04-11 22:55:41 -0500

    Adding accordion

--- src/components/services.js
+++ src/components/services.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import Collapse from '@kunukn/react-collapse';
+
+
 
 class Services extends Component {
     render() {
@@ -7,15 +10,21 @@ class Services extends Component {
             <div>
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                    <h2><u>How’s Your PMO?</u></h2>
+                        <Collapse className='collapse-css-transition' isOpen={false || true}>
+                            <p>Paragraph of text</p>
+                            <p>Another paragraph is also OK</p>
+                            <p>Images and any other content are ok too</p>
+                            <img src="cutecat.gif" />
+                        </Collapse>
+                        <h2><u>How’s Your PMO?</u></h2>
 
-                    <h2><u>Project Initiation and Readiness</u></h2>
-                    <h2><u>Project Planning</u></h2>
+                        <h2><u>Project Initiation and Readiness</u></h2>
+                        <h2><u>Project Planning</u></h2>
 
-                    <h2><u> Project Scheduling</u></h2>
+                        <h2><u> Project Scheduling</u></h2>
 
-                    <h2><u> Project Execution</u></h2>
-                    <h2><u>Project Completion and Documentation</u></h2>
+                        <h2><u> Project Execution</u></h2>
+                        <h2><u>Project Completion and Documentation</u></h2>
                     </Cell>
                 </Grid>
             </div>
