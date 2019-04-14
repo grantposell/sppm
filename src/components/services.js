f7ce61786cded85ee592d193c584987e0f224fc8
grantposell <grantposell@gmail.com>
2019-04-13 22:16:30 -0500

    Fixed styling of accordion and titles

--- src/components/services.js
+++ src/components/services.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
-import cx from "classnames";
+import cx from 'classnames';
 import Collapse from '@kunukn/react-collapse';
 
 
@@ -44,74 +44,158 @@ class Services extends Component {
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
-            <img
-              className="image"
-              alt="random"
-              src="https://source.unsplash.com/user/erondu/500x200"
-            />
-            <button onClick={() => this.toggle(1)} className="app__button">
-              close
-            </button>
           </div>
         </Collapse>
-
         <button
           className={cx("app__toggle", {
             "app__toggle--active": this.state.isOpen2
           })}
           onClick={() => this.toggle(2)}
         >
-          toggle
+          <span className="app__toggle-text">Project Initiation and Readiness</span>
+          <div className="rotate90">
+            <svg
+              className={cx("icon", { "icon--expanded": this.state.isOpen1 })}
+              viewBox="6 0 12 24"
+            >
+              <polygon points="8 0 6 1.8 14.4 12 6 22.2 8 24 18 12" />
+            </svg>
+          </div>
         </button>
-
         <Collapse
           isOpen={this.state.isOpen2}
           className={
             "app__collapse app__collapse--gradient " +
             (this.state.isOpen2 ? "app__collapse--active" : "")
           }
-          transition="height 800ms cubic-bezier(0.4, 0, 0.2, 1)"
-          aria-hidden={this.state.isOpen2 ? "false" : "true"}
-          elementType="article"
-          render={collapseState => (
-            <React.Fragment>
-              <div className="app__content">
-                <div>{collapseState}</div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-                <img
-                  className="image"
-                  alt="random"
-                  src="https://source.unsplash.com/user/erondu/500x200"
-                />
-                <button onClick={() => this.toggle(2)} className="app__button">
-                  close
-                </button>
-              </div>
-            </React.Fragment>
-          )}
-        />
-        <Grid className="landing-grid">
-          <Cell col={12}>
-            <Collapse className='collapse-css-transition' isOpen={false || true}>
-              <p>Paragraph of text</p>
-              <p>Another paragraph is also OK</p>
-              <p>Images and any other content are ok too</p>
-              <img src="cutecat.gif" />
-            </Collapse>
-            <h2><u></u></h2>
-
-            <h2><u>Project Initiation and Readiness</u></h2>
-            <h2><u>Project Planning</u></h2>
-
-            <h2><u> Project Scheduling</u></h2>
-
-            <h2><u> Project Execution</u></h2>
-            <h2><u>Project Completion and Documentation</u></h2>
-          </Cell>
-        </Grid>
+        >
+          <div className="app__content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </div>
+        </Collapse>
+        <button
+          className={cx("app__toggle", {
+            "app__toggle--active": this.state.isOpen3
+          })}
+          onClick={() => this.toggle(3)}
+        >
+          <span className="app__toggle-text">Project Planning</span>
+          <div className="rotate90">
+            <svg
+              className={cx("icon", { "icon--expanded": this.state.isOpen3 })}
+              viewBox="6 0 12 24"
+            >
+              <polygon points="8 0 6 1.8 14.4 12 6 22.2 8 24 18 12" />
+            </svg>
+          </div>
+        </button>
+        <Collapse
+          isOpen={this.state.isOpen3}
+          className={
+            "app__collapse app__collapse--gradient " +
+            (this.state.isOpen3 ? "app__collapse--active" : "")
+          }
+        >
+          <div className="app__content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </div>
+        </Collapse>
+        <button
+          className={cx("app__toggle", {
+            "app__toggle--active": this.state.isOpen4
+          })}
+          onClick={() => this.toggle(4)}
+        >
+          <span className="app__toggle-text">Project Scheduling</span>
+          <div className="rotate90">
+            <svg
+              className={cx("icon", { "icon--expanded": this.state.isOpen4 })}
+              viewBox="6 0 12 24"
+            >
+              <polygon points="8 0 6 1.8 14.4 12 6 22.2 8 24 18 12" />
+            </svg>
+          </div>
+        </button>
+        <Collapse
+          isOpen={this.state.isOpen4}
+          className={
+            "app__collapse app__collapse--gradient " +
+            (this.state.isOpen4 ? "app__collapse--active" : "")
+          }
+        >
+          <div className="app__content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </div>
+        </Collapse>
+        <button
+          className={cx("app__toggle", {
+            "app__toggle--active": this.state.isOpen5
+          })}
+          onClick={() => this.toggle(5)}
+        >
+          <span className="app__toggle-text">Project Execution</span>
+          <div className="rotate90">
+            <svg
+              className={cx("icon", { "icon--expanded": this.state.isOpen4 })}
+              viewBox="6 0 12 24"
+            >
+              <polygon points="8 0 6 1.8 14.4 12 6 22.2 8 24 18 12" />
+            </svg>
+          </div>
+        </button>
+        <Collapse
+          isOpen={this.state.isOpen5}
+          className={
+            "app__collapse app__collapse--gradient " +
+            (this.state.isOpen5 ? "app__collapse--active" : "")
+          }
+        >
+          <div className="app__content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </div>
+        </Collapse>
+        <button
+          className={cx("app__toggle", {
+            "app__toggle--active": this.state.isOpen6
+          })}
+          onClick={() => this.toggle(6)}
+        >
+          <span className="app__toggle-text">Project Completion and Documentation</span>
+          <div className="rotate90">
+            <svg
+              className={cx("icon", { "icon--expanded": this.state.isOpen4 })}
+              viewBox="6 0 12 24"
+            >
+              <polygon points="8 0 6 1.8 14.4 12 6 22.2 8 24 18 12" />
+            </svg>
+          </div>
+        </button>
+        <Collapse
+          isOpen={this.state.isOpen6}
+          className={
+            "app__collapse app__collapse--gradient " +
+            (this.state.isOpen6 ? "app__collapse--active" : "")
+          }
+        >
+          <div className="app__content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </div>
+        </Collapse>
       </div>
     )
   }
